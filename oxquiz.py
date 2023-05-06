8431a246c1d588e69b06d0739c4a953c9957b664
zinsss <87020914+zinsss@users.noreply.github.com>
2023-05-06 14:46:22 +0900

    updated questions

--- oxquiz.py
+++ oxquiz.py
@@ -33,7 +33,7 @@ QUESTIONS = [
     ["엄마 이름은 차영.", True, "O", ""],
     ["엄마 차 파란색.", True, "O", ""],
     ["만촌동에 할머니 할아버지가 있어요.", True, "O", ""],
-    ["아빠 이름은 이준성.", True, "X", "아빠 이름은 이진성이에요.."],
+    ["아빠 이름은 이준성.", True, "X", "아빠 이름은 이진성이에요."],
     ["1은 영어로 two.", True, "X", "1은 영어로 원.이에요."],
     ["아빠 차는 LEXUS", True, "X", "아빠 차는 미니에요."],
     ["로운이는 포항제일유치원을 다녀요.", True, "O", ""],
@@ -43,6 +43,19 @@ QUESTIONS = [
     ["유치원 선생님 이름은 이유리.", True, "O", ""],
     ["사과는 보라색이에요.", True, "X", "사과는 빨간색이에요."],
     ["Two plus one equals, five.", False, "X", "Two plus one equals three."],
+    ["만촌동 할머니집은 8층이에요", True, "X", "만촌동 할머니집은 15층이에요"],
+    ["이모 이름은 김지혜.", True, "O", ""],
+    ["로운이는 경선어린이집을 다녔어요.", True, "O", ""],
+    ["율이 누나는 서울에 살아요.", True, "O", ""],
+    ["로운이 집은 103동 1205호에요.", True, "X", "103동 1505호에요."],
+    ["사과는 빨간색이에요.", True, "O", ""],
+    ["해안로1774번길 51번지에는 오도리집이 있어요.", True, "O", ""],
+    ["아빠 직업은 경찰이에요.", True, "X", "아빠는 의사에요."],
+    ["엄마는 머리가 길어요.", True, "O", ""],
+    ["오도리집에는 2층 침대가 있어요.", True, "O", ""],
+    ["기도가 끝나면 히멘이라고 말해요.", True, "X", "아멘이라고 말해요."],
+    ["루돌프 사슴코는 노란색이에요.", True, "X", "매우 반짝이는 빨간코에요"],
+    ["큰아빠집에 가면 강아지 쪼코가 있어요.", True, "O", ""], 
 ]
 
 quiz = []
@@ -60,7 +73,7 @@ def start_game(ui):
     ui.O_btn.setStyleSheet(O_BTN_STYLE)
     ui.X_btn.setStyleSheet(X_BTN_STYLE)
 
-    # utils.play_tts(quiz[0], kr=quiz[1])
+    utils.play_tts(quiz[0], kr=quiz[1])
 
     ui.ox_start.setEnabled(False)
     ui.ox_tts.setEnabled(True)
